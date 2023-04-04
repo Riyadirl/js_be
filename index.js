b1951853a3feba382c833650d49172053b752dd5
Mahbub Hasaan Riyad <riyad.info1@gmail.com>
2023-04-05 01:29:13 +0600

    ..

--- index.js
+++ index.js
@@ -1,2 +1,27 @@
-console.log("mahbub hasan riyad")
-document.write("Mahbub hasan")
+console.log("mahbub hasan riyad");
+document.write("Mahbub hasan");
+
+// Query Selector
+/*
+<body>
+
+        <h1 id="myId">h1</h1>
+        <h4>h4</h4>
+        <h6 class="myClass">h6</h6>
+        <p name="myName">p</p>
+        <button ondblclick="FindEle()">Click</button>
+
+        <script>
+            function FindEle(){
+                document.querySelector('h4').innerHTML ="h4 manupulated"
+                document.querySelector('.myClass').innerHTML ="h6 manupulated"
+                document.querySelector('#myId').innerHTML ="h1 manupulated"
+                document.querySelector('p[name="myName"]').innerHTML ="p manupulated"
+            }
+  
+        </script>
+*/
+
+//Ajax
+
+/* */
