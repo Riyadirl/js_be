b57ac0a58b4a01a39861abdfa234d0cb89caffaf
Mahbub Hasaan Riyad <riyad.info1@gmail.com>
2023-04-05 02:50:49 +0600

    es6

---
+++ ES6.js
@@ -0,0 +1,29 @@
+//ECMAScript 6
+
+//1 hello world
+//console.log("hello world");
+
+//2 strict
+/*"use strict";
+function myName() {
+  name = "Riyad";
+}
+myName();*/
+
+//3 with spread function
+//let poorC = ["Bd", "Sri"];
+//let richC = [...poorC,"USA", "Canada", "Italy"];
+//console.log(richC);
+
+// without spread function
+//richC.push(poorC);
+
+//4   rest parameters
+function calculate(...numbers) {
+  let sum = 0;
+  for (let i of numbers) {
+    sum = sum + i;
+  }
+  console.log(sum);
+}
+calculate(1, 2, 3, 4, 5, 3, 2, 4);
